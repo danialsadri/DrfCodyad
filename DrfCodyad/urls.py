a5e1e43162fceb2822bbf57338f5582ee466cf07
danial sadri <danielsadri01@gmail.com>
2023-09-26 20:18:16 +0330

    add swagger ui

--- DrfCodyad/urls.py
+++ DrfCodyad/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
 urlpatterns = [
     path('api/admin/', admin.site.urls),
-    path('api/auth/', include('rest_framework.urls')),
     path('api/', include('home.urls', namespace='home')),
+    path('api/schema/file/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
