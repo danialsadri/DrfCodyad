978a44cfb6e9923ed4a03823359ab1b6c3ebbc00
danial sadri <danielsadri01@gmail.com>
2023-09-26 21:50:36 +0330

    add validate_title and validate func for ArticleSerializer

--- home/serializers.py
+++ home/serializers.py
@@ -15,3 +15,13 @@ class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = "__all__"
+
+    def validate_title(self, value):
+        if value == 'hello world':
+            raise serializers.ValidationError('you can not choose a hello world')
+        return value
+
+    def validate(self, attrs):
+        if attrs['title'] == attrs['description']:
+            raise serializers.ValidationError('title and description can not be same')
+        return attrs
