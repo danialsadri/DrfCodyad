d9a272c421bbcd724c0d9de12af3c055b5d30191
danial sadri <danielsadri01@gmail.com>
2023-09-26 19:50:47 +0330

    add ArticleCreateView and ArticleUpdateView and ArticleDeleteView

--- home/urls.py
+++ home/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('user/list/', views.UserListView.as_view()),
     path('article/list/', views.ArticleListView.as_view()),
     path('article/detail/<int:post_id>/', views.ArticleDetailView.as_view()),
+    path('article/create/', views.ArticleCreateView.as_view()),
+    path('article/update/<int:post_id>/', views.ArticleUpdateView.as_view()),
+    path('article/delete/<int:post_id>/', views.ArticleDeleteView.as_view()),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.request import Request
@@ -33,18 +34,44 @@ class UserListView(APIView):
     def get(self, request: Request):
         users = User.objects.all()
         user_serializer = UserSerializer(instance=users, many=True)
-        return Response(data=user_serializer.data)
+        return Response(data=user_serializer.data, status=status.HTTP_200_OK)
 
 
 class ArticleListView(APIView):
     def get(self, request: Request):
         articles = Article.objects.all()
         article_serializer = ArticleSerializer(instance=articles, many=True)
-        return Response(data=article_serializer.data)
+        return Response(data=article_serializer.data, status=status.HTTP_200_OK)
 
 
 class ArticleDetailView(APIView):
     def get(self, request: Request, post_id):
         article = Article.objects.get(id=post_id)
         article_serializer = ArticleSerializer(instance=article)
-        return Response(data=article_serializer.data)
+        return Response(data=article_serializer.data, status=status.HTTP_200_OK)
+
+
+class ArticleCreateView(APIView):
+    def post(self, request: Request):
+        article_serializer = ArticleSerializer(data=request.data)
+        if article_serializer.is_valid():
+            article_serializer.save()
+            return Response(data=article_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(data=article_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ArticleUpdateView(APIView):
+    def put(self, request: Request, post_id):
+        article = Article.objects.get(id=post_id)
+        article_serializer = ArticleSerializer(instance=article, data=request.data, partial=True)
+        if article_serializer.is_valid():
+            article_serializer.save()
+            return Response(data=article_serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(data=article_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ArticleDeleteView(APIView):
+    def delete(self, request: Request, post_id):
+        article = Article.objects.get(id=post_id)
+        article.delete()
+        return Response(data=None, status=status.HTTP_204_NO_CONTENT)
