d314938a5a47939f15a8d4ac92d9d967d1061038
danial sadri <danielsadri01@gmail.com>
2023-09-27 17:30:36 +0330

    add model Admin

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, BlockUser
 
 
 @admin.register(Article)
@@ -7,7 +7,7 @@ class ArticleAdmin(admin.ModelAdmin):
     list_display = ['title', 'created', 'status']
 
 
+@admin.register(BlockUser)
 class BlockUserAdmin(admin.ModelAdmin):
     list_display = ['username']
     list_filter = ['username']
-    list_editable = ['username']
